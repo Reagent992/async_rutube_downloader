d88ff79d6701078c20538f070c1681c187fe31d1
Miron Sadykov <MironSadykov@yandex.ru>
2024-11-18 22:12:28 +0300

    refactor

--- docs/README.md
+++ docs/README.md
@@ -1,5 +1,8 @@
-# Rutube downloader
+English / [Russian](./README_RU.md)
+
 > This project was created for learning purposes and was inspired by a similar synchronous library.
+
+- The async version allows you to use the full speed of your Internet connection.
 
 # used libs
 - [m3u8](https://github.com/globocom/m3u8/)
--- src/main.py
+++ src/main.py
@@ -5,14 +5,12 @@ from typing import Any, Final
 
 import aiofiles
 import m3u8
-from aiohttp import ClientSession
+from aiohttp import ClientError, ClientSession
 
 from config import CHUNK_SIZE, RUTUBE_API_LINK, VIDEO_ID_REGEX
 from playlist import MasterPlaylist
 
-LINK: Final = (
-    "https://rutube.ru/video/a684a67d21eda3792baf1ec433ab653a/"  # 7 минут
-)
+LINK: Final = "https://rutube.ru/video/a684a67d21eda3792baf1ec433ab653a/"  # 7 минут  # downloaded for ~43 seconds.
 # LINK: Final = (
 #     "https://rutube.ru/video/940418fbd25740b72410070f540b0cde/"  # 23 минуты
 # )
@@ -21,63 +19,72 @@ LINK: Final = (
 # )
 
 
-async def _get_api_response(session: ClientSession, id: str) -> dict[str, Any]:
-    async with session.get(RUTUBE_API_LINK.format(id)) as result:
-        result.raise_for_status()
-        return await result.json()
+async def _get_api_response(
+    session: ClientSession, video_id: str
+) -> dict[str, Any]:
+    try:
+        async with session.get(RUTUBE_API_LINK.format(video_id)) as result:
+            result.raise_for_status()
+            return await result.json()
+    except ClientError as e:
+        print(f"Error fetching API response: {e}")
+        raise ClientError
 
 
-def _get_video_id(url: str) -> str:
+def extract_video_id(url: str) -> str:
     if result := re.search(VIDEO_ID_REGEX, url):
         return result.group()
-    else:
-        raise ValueError("Wrong url")
+    raise ValueError(f"Invalid Rutube URL: {url}")
+
+
+def extract_title(api_response: dict[str, Any]) -> str:
+    video_title = api_response.get("title", "Unknown")
+    sanitized_title = re.sub(r"[^\w\-_\. ]", "_", video_title)
+    return f"{sanitized_title}.mp4"
 
 
 async def _download_segment(
     session: ClientSession, segment: m3u8.Segment
 ) -> bytes:
-    async with session.get(segment.absolute_uri) as response:
-        response.raise_for_status()
-        return await response.read()
+    try:
+        async with session.get(segment.absolute_uri) as response:
+            response.raise_for_status()
+            return await response.read()
+    except ClientError as e:
+        print(f"Error downloading segment {segment.absolute_uri}: {e}")
+        raise ClientError
 
 
-async def write_to_file(segment_data: bytes, file_name: str) -> None:
-    async with aiofiles.open(file_name, mode="ab") as file:
-        await file.write(segment_data)
-
-
-async def download(
+async def download_video(
     stream: m3u8.M3U8, session: ClientSession, video_title: str
 ) -> None:
     segments = list(stream.segments)
     file_name = f"{video_title}.mp4"
 
-    # Process segments in chunks
-    for i in range(0, len(segments), CHUNK_SIZE):
-        tasks = []
-        for segment in segments[i : i + CHUNK_SIZE]:
-            task = asyncio.create_task(_download_segment(session, segment))
-            tasks.append(task)
+    async with aiofiles.open(file_name, mode="wb") as file:
+        for i in range(0, len(segments), CHUNK_SIZE):
+            download_tasks = [
+                asyncio.create_task(_download_segment(session, segment))
+                for segment in segments[i : i + CHUNK_SIZE]
+            ]
 
-        downloaded_segments = await asyncio.gather(*tasks)
+            downloaded_segments = await asyncio.gather(*download_tasks)
 
-        # Write downloaded segments asynchronously
-        write_tasks = [
-            asyncio.create_task(write_to_file(data, file_name))
-            for data in downloaded_segments
-        ]
-        await asyncio.gather(*write_tasks)
+            for segment_data in downloaded_segments:
+                await file.write(segment_data)
 
 
 async def main(link: str) -> None:
-    id = _get_video_id(link)
-    async with ClientSession() as session:
-        api_response = await _get_api_response(session, id)
-        video_title = api_response.get("title", "Unknown")
-        playlist = MasterPlaylist(api_response)
-        stream = m3u8.load(playlist.get_best_quality().uri)
-        await download(stream, session, video_title)
+    try:
+        id = extract_video_id(link)
+        async with ClientSession() as session:
+            api_response = await _get_api_response(session, id)
+            video_title = extract_title(api_response)
+            playlist = MasterPlaylist(api_response)
+            stream = m3u8.load(playlist.get_best_quality().uri)
+            await download_video(stream, session, video_title)
+    except Exception as e:
+        print(f"Unexpected error during video download: {e}")
 
 
 if __name__ == "__main__":
