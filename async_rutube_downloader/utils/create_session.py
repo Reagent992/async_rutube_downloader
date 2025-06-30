6e4c98f466af4816bb20db193380d067533ed091
Miron Sadykov <MironSadykov@yandex.ru>
2025-06-30 14:53:42 +0300

    fix(network): ensure SSL works in PyInstaller build using certifi bundle

--- async_rutube_downloader/utils/create_session.py
+++ async_rutube_downloader/utils/create_session.py
@@ -1,6 +1,8 @@
+import ssl
 from asyncio import AbstractEventLoop
 
-from aiohttp import ClientSession, ClientTimeout
+import certifi
+from aiohttp import ClientSession, ClientTimeout, TCPConnector
 
 from async_rutube_downloader.utils.miscellaneous import get_or_create_loop
 
@@ -24,8 +26,15 @@ def create_aiohttp_session(
         # Chunks are small in size,
         # so 3 minutes should be enough to download one chunk.
     )
+
+    # Fixes [SSL: CERTIFICATE_VERIFY_FAILED] in PyInstaller builds,
+    # especially on GitHub Actions, where system CA certs may be missing.
+    # certifi provides a portable CA bundle for reliable HTTPS.
+    ssl_context = ssl.create_default_context(cafile=certifi.where())
+    connector = TCPConnector(ssl=ssl_context, loop=loop)
     return ClientSession(
         loop=loop,
         timeout=session_timeout,
+        connector=connector,
         raise_for_status=True,
     )
